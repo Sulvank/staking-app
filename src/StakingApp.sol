9b7d5de4ef42162bb2c65d282c0f38120393c36d
Samuel Casanova Valdés <samuel@distritolegal.es>
2025-04-20 21:51:22 +0100

    feat: adding new functionalities for claim rewards

--- src/StakingApp.sol
+++ src/StakingApp.sol
@@ -9,25 +9,29 @@ contract StakingApp is Ownable {
 
     address public stakingToken;
     uint256 public stakingPeriod;
-    uint256 public fixedStackinAmount;
+    uint256 public fixedStackingAmount;
+    uint256 public rewardPerPeriod; // 1 ether = 1 token
     mapping(address => uint256) public userBalance;
+    mapping(address => uint256) public elapsePeriod;
 
     event ChangeStakingPeriod(uint256 newStakingPeriod_);
     event DepositTokens(address userAddress_, uint256 depositAmount_);
     event WithdrawTokens(address userAddress_, uint256 withdrawAmount_);
 
-    constructor(address stakingToken_, address owner_, uint256 stakingPeriod_, uint256 fixedStakingAmount_) Ownable(owner_) {
+    constructor(address stakingToken_, address owner_, uint256 stakingPeriod_, uint256 fixedStakingAmount_, uint256 rewardPerPeriod_) Ownable(owner_) {
         stakingToken = stakingToken_;
         stakingPeriod = stakingPeriod_;
-        fixedStackinAmount = fixedStakingAmount_;
+        fixedStackingAmount = fixedStakingAmount_;
+        rewardPerPeriod = rewardPerPeriod_;
     }
 
     function depositTokens(uint256 tokenAmountToDeposit_) external {
-        require(tokenAmountToDeposit_ == fixedStackinAmount, "Deposit amount must be 10 tokens");
+        require(tokenAmountToDeposit_ == fixedStackingAmount, "Deposit amount must be 10 tokens");
         require(userBalance[msg.sender] == 0, "You already have a deposit");
 
         IERC20(stakingToken).transferFrom(msg.sender, address(this), tokenAmountToDeposit_);
         userBalance[msg.sender] += tokenAmountToDeposit_;
+        elapsePeriod[msg.sender] = block.timestamp;
 
         emit DepositTokens(msg.sender, tokenAmountToDeposit_);
     }
@@ -42,12 +46,22 @@ contract StakingApp is Ownable {
     }
 
 
+    function claimRewards() external {
+        require(userBalance[msg.sender] == fixedStackingAmount, "You have no deposit to claim rewards for");
+
+        uint256 elapsePeriod_ = block.timestamp - elapsePeriod[msg.sender];
+        require(elapsePeriod_ >= stakingPeriod, "Staking period not yet completed");
+
+        elapsePeriod[msg.sender] = block.timestamp;
+
+        (bool success,) = msg.sender.call{value: rewardPerPeriod}("");
+        require(success, "Transfer failed.");
+    }
+
+
     function changeStakingPeriod(uint256 newStakingPeriod_) external onlyOwner {
         stakingPeriod = newStakingPeriod_;
         emit ChangeStakingPeriod(newStakingPeriod_);
     }
-
-
-
 }
 
